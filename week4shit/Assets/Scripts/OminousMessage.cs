b9f4c768f7f68ef098512087ee891f6651a41315
bunniay <bunniaybb@gmail.com>
2025-09-17 14:05:18 -0400

    demo

---
+++ week4shit/Assets/Scripts/OminousMessage.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+
+public class OminousMessage : MonoBehaviour
+{
+    public TMP_Text messageText;
+    public List<string> ominousMessages;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ScaryMessages ()
+    {
+
+    }
+}
