527a24537dd5fd362929e38c152a1722cf699acb
bunniay <bunniaybb@gmail.com>
2025-09-17 14:41:51 -0400

    demo2

--- week4shit/Assets/Scripts/OminousMessage.cs
+++ week4shit/Assets/Scripts/OminousMessage.cs
@@ -2,11 +2,13 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OminousMessage : MonoBehaviour
 {
     public TMP_Text messageText;
     public List<string> ominousMessages;
+    public string displayedMsg;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Start()
     {
@@ -20,6 +22,19 @@ public class OminousMessage : MonoBehaviour
     }
     public void ScaryMessages ()
     {
+        //two ways to write this
 
+        //NUMBER 1
+
+        //displayedMsg = ominousMessages[Random.Range(0,ominousMessages.Count)];
+        //messageText.text = displayedMsg;
+
+        //NUMBER 2
+        messageText.text = ominousMessages[Random.Range(0, ominousMessages.Count)];
+    }
+
+    public void NewScene()
+    {
+        SceneManager.LoadScene(1);
     }
 }
